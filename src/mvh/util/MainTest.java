d380dc8a573424557d64f2037c7387b7c3736ebe
Abhay Chopra <abhay.chopra1@ucalgary.ca>
2022-03-09 15:37:01 -0700

    Added test cases for loadWorld() from Reader.java

--- src/mvh/util/MainTest.java
+++ src/mvh/util/MainTest.java
@@ -1,28 +1,76 @@
 package mvh.util;
 
+import mvh.world.Entity;
 import org.junit.jupiter.api.*;
+
+import java.io.File;
 
 class MainTest {
 
+    //Initializing Strings needed in tests
+    String basicWorld;
+    String complexWorld;
+
+    /**
+     * Basic structure setup to visualize the different worlds (created via being given different files)
+     */
     @BeforeEach
     void setUp() {
-
+        basicWorld =
+                """
+                #####
+                #M..#
+                #...#
+                #..H#
+                #####
+                NAME   \tS\tH\tSTATE\tINFO
+                Mons(1)\tM\t10\tALIVE\tSWORD
+                Hero(2)\tH\t10\tALIVE\t3\t1
+                """;
+        complexWorld =
+                """
+                ######
+                #A.B.#
+                #..C.#
+                #....#
+                #....#
+                #.DEF#
+                ######
+                NAME   \tS\tH\tSTATE\tINFO
+                Mons(1)\tA\t10\tALIVE\tAXE
+                Mons(2)\tB\t9\tALIVE\tSWORD
+                Mons(3)\tC\t8\tALIVE\tCLUB
+                Hero(4)\tD\t8\tALIVE\t5\t2
+                Hero(5)\tE\t7\tALIVE\t4\t1
+                Hero(6)\tF\t6\tALIVE\t3\t1
+                """;
     }
 
     /**
-     *
+     * Reset entity counter as each test is likely creating some new world where entities are created
      */
-    @Test
-    void loadWorldFailTest() {
-
+    @AfterEach
+    void tearDown(){
+        Entity.resetIDCounter();
     }
 
     /**
-     *
+     * Testing whether we can produce an identical world (relative to expected) given a basic world file.
      */
     @Test
-    void loadWorldCorrectEntries(){
+    void loadWorldBasicWorld() {
+        File file = new File("world.txt");
+        Assertions.assertEquals(basicWorld, Reader.loadWorld(file).toString());
+    }
 
+    /**
+     * Testing whether we can produce an identical world given a larger world (as compared to last test case) with
+     * multiple entities (and entities of the same type)
+     */
+    @Test
+    void loadWorldBigWorld(){
+        File file = new File("worldBig.txt");
+        Assertions.assertEquals(complexWorld, Reader.loadWorld(file).toString());
     }
 
     /**
